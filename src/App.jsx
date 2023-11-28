09387fbfc0799041458e97977955b6656f231aba
Mobaraque7 <mk2956806@gmail.com>
2023-11-28 07:58:02 +0600

    Header styleing

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,20 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import Signout from './pages/Signout';
+import Header from './Components/Header';
 export default function App() {
   return (
     <>
-    <h1 className="text-red-500 font-bold underline">
-      Hello world!
-    </h1>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='/sign-out' element={<Signout />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-out' element={<Signout />} />
+        </Routes>
+      </BrowserRouter>
     </>
     
   )
